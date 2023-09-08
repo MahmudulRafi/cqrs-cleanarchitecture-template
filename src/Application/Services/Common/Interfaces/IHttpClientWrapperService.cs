dce6681cd7567b575eeebd80481a05bb47dcdc2a
MahmudulRafi <rafi.mahmudul.h@gmail.com>
2023-09-08 15:42:04 +0600

    Http wrapper service added & namespace modified

---
+++ src/Application/Services/Common/Interfaces/IHttpClientWrapperService.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Services.Common.Interfaces
+{
+    public interface IHttpClientWrapperService
+    {
+        Task<TResponse> GetAsync<TResponse>(string url, string accessToken);
+        Task<TResponse> PostAsync<TResponse, TContent>(string url, string accessToken, TContent postObject);
+    }
+}
