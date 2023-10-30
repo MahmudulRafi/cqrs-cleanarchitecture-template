4715824bebf370ef1c6a0a7133695bc7978eac48
MahmudulRafi <rafi.mahmudul.h@gmail.com>
2023-10-30 12:39:32 +0600

    added dummy user seed data

--- src/Infrastructure/Data/Configurations/UserConfiguration.cs
+++ src/Infrastructure/Data/Configurations/UserConfiguration.cs
@@ -5,7 +5,8 @@
         public void Configure(EntityTypeBuilder<User> builder)
         {
             builder.HasData(
-                new User { Name = "Mahmudul Rafi", Email = "test@gmail.com", Phone = "0121231232" }
+                new User { Name = "Mahmudul Rafi", Email = "test@gmail.com", Phone = "0121231232" },
+                new User { Name = "Mahadi Hasan Rafat", Email = "test1@gmail.com", Phone = "012123324" }
                 );
         }
     }
