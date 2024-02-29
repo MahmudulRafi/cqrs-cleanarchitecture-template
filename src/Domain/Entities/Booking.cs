8263ca4cf6e348295cad10000a646117127ecf1b
MahmudulRafi <rafi.mahmudul.h@gmail.com>
2024-02-29 10:37:45 +0600

    Foreignkey added

--- src/Domain/Entities/Booking.cs
+++ src/Domain/Entities/Booking.cs
@@ -1,10 +1,18 @@
-﻿namespace Domain.Entities
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities
 {
     public class Booking : BaseEntity
     {
-        public string UserId { get; set; } = string.Empty;  
+        [ForeignKey("User")]
+        public string UserId { get; set; } = string.Empty;
+        [ForeignKey("Event")]
         public string EventId { get; set; } = string.Empty;
         public bool IsConfirmed { get; set; } = default;
         public bool IsCanceledByUser { get; set; } = default;
+
+        // Navigation propertise
+        public User User { get; set; }
+        public Event Event { get; set; }
     }
 }
--- src/Domain/Entities/Organization.cs
+++ src/Domain/Entities/Organization.cs
@@ -1,11 +1,17 @@
-﻿namespace Domain.Entities
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities
 {
     public class Organization : BaseEntity
     {
         public string Name { get; set; } = string.Empty;
+        [ForeignKey("User")]
         public string ReportingUserId { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Phone { get; set; } = string.Empty;
+
+        // Navigation propertise
+        public User User { get; set; }  
 
     }
 }
