5912b7113f39884e39585703ce4a5da6eab5eed8
MahmudulRafi <rafi.mahmudul.h@gmail.com>
2024-02-29 10:37:45 +0600

    Foreignkey added

--- src/Domain/Entities/Event.cs
+++ src/Domain/Entities/Event.cs
@@ -1,15 +1,21 @@
-﻿namespace Domain.Entities
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities
 {
     public class Event : BaseEntity
     {
         public string Title { get; set; } = string.Empty;
         public DateTime EventStartDate { get; set; } = DateTime.Now;
         public DateTime EventEndDate { get; set; } = DateTime.Now;
+        [ForeignKey("Organization")]
         public string OrganizationId { get; set; } = string.Empty;
         public string OrganizedByUserId { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string Location { get; set; } = string.Empty;
         public int SeatAllocation { get; set; } = default;
         public bool IsAvailableForRegistraton { get; set; } = default;
+
+        // Navigation propertise
+        public Organization Organization { get; set; }  
     }
 }
